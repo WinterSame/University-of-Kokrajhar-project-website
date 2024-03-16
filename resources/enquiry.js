fcbca6ab9f12140a5cf4fa52b94088545c81af7a
Same Winter <127609471+SameWinter@users.noreply.github.com>
2024-03-16 12:43:31 +0530

    added final

---
+++ resources/enquiry.js
@@ -0,0 +1,22 @@
+.click-me{
+    display:block;
+    width:160px;
+    height:50px;
+    background: #dde1e7;
+    text-align:center;
+    font-size:22px;
+    line-height:50px;
+    cursor:pointer;
+    box-shadow: -3px -3px 7px #fffdfdcc,
+                3px 3px 5px rgba(94, 104, 121, 0.342);
+  }
+  .click-me:hover{
+    color: #08adef;
+    box-shadow: inset 2px 2px 5px #babecc,
+                inset -5px -5px 10px #ffffff73;
+  }
+  The following little code has been used to hide the checkbox. I have used display: none here so the checkbox will be hidden.
+  
+  #click{
+    display:none;
+  }
